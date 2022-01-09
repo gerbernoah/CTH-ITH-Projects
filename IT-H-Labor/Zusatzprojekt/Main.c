3f636fcc51707c04f491a93225b71cfc2af9d33d
Noah.G <47790983+hallojuhu@users.noreply.github.com>
2022-01-09 21:26:57 +0100

    Zusatzprojekt added remove funcitno

--- IT-H-Labor/Zusatzprojekt/Main.c
+++ IT-H-Labor/Zusatzprojekt/Main.c
@@ -6,20 +6,36 @@ int count;
 void Main()
 {
 	count = P2 = IT0 = 0;
-	IE = 0x81;
+	IE = 0x85;
 	IT0 = 1;
+	IT1 = 1;
 	while(1);
 }
 
-void Ir() interrupt 0	//Externe interrupt 0 an port 3.2
+void Ausgabe()
 {
 	int tens, ones;
-	count++;
-	if(count > 99)
-		return;
 	tens = count/10;
 	ones = count - tens*10;
 	
 	P2 = tens << 4;
 	P2 += ones;
 }
+
+void Ir1() interrupt 0	//Externe interrupt 0 an port 3.2
+{
+	if(count > 98)
+		return;
+	count++;
+	Ausgabe();
+}
+
+void Ir2() interrupt 2	//Externe interrupt 2 an port 3.3
+{
+	
+	if(count == 0)
+		return;
+	count--;
+	Ausgabe();
+}
+
