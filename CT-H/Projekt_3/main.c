a71ed722c80753c95a0019ae46df451e38571237
Noah Gerber <47790983+hallojuhu@users.noreply.github.com>
2022-05-19 14:34:59 +0200

    Added CTH 3 Code

---
+++ CT-H/Projekt_3/main.c
@@ -0,0 +1,65 @@
+#include <reg51.h>
+#include <stdio.h>
+
+unsigned int ziffer[] = {0, 8, 1, 5};
+int interruptCount;
+int index = 0;
+int end = 1;
+int count;
+sbit impuls = P2^0;
+sbit schalter = P2^1;
+sbit notfall = P2^2;
+
+void init() {
+	TMOD = 0x10;
+	IE = 0x88;
+	P2 = 0;
+}
+
+void timerInterrupt() interrupt 3
+{
+	TR1 = 0; // stoppt den timer
+  TH1 = 0x3C;         // 50 ms
+	TL1 = 0xB0;
+	interruptCount++;
+	TR1 = 1; // startet den timer
+}
+
+void delay(float seconds)
+{
+	TR1 = 1; // startet den timer
+	while (interruptCount < seconds * 20);
+	interruptCount = 0;
+	TR1 = 0; // stoppt den timer
+}
+
+void main() {
+	init();
+	while (end) {
+		while(schalter == 0);
+		if (P1 != 0xFF)
+			continue;
+			
+		if (ziffer[index] == 0)
+			count = 10;
+		else
+			count = ziffer[index];
+		count = count * 2;
+	}
+	
+	for (;count > 0; count--)
+	{
+		impuls = 1;
+		delay(0.2);
+		impuls = 1;
+		delay(0.2);
+	}
+		if (index == 4) {
+			end = 0;
+			notfall = 1;
+		}
+	while(1);
+}
+
+
+
